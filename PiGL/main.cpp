1dc3c45002eda964951f8ae0222ea3c9500b0d00
5aitama <alex.code@icloud.com>
2019-07-11 23:33:54 +0200

    Initial Commit

---
+++ PiGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PiGL
+//
+//  Created by Alex Sb on 11/07/2019.
+//  Copyright © 2019 Alex Sb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
